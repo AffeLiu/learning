9f9c932d9acb49d9b372999ef9d060409e19cfbf
Affe <qwgygu@gmail.com>
2017-11-02 00:23:13 +0800

    Day8-L8

---
+++ Udemy/L8.2-JS-stalker exercise.js
@@ -0,0 +1,16 @@
+var firstName=prompt("What's your first name?");
+
+var lastName=prompt("what's your last name?");
+
+var age=prompt("How old are you?");
+
+var fullName=firstName + " " + lastName;
+
+
+console.log("Your full name is " + fullName);
+
+console.log("You are " + age + " years old");
+
+
+
+
