e879aecb20c753f443e3b55ce46b44b59f4bf6af
Affe <qwgygu@gmail.com>
2017-11-01 19:48:35 +0800

    Day8-L8

--- Udemy/L8-JSintro.js
+++ Udemy/L8-JSintro.js
@@ -27,4 +27,36 @@ String （字串）：可相加 雙引號單引號皆可，但不可混用
 				"good bye".length ＝８ (空白鍵也會計算計進去)
 
 				顯示字串之第n個數 從０開始算
+
 				"hello"[0] ＝０
+
+
+
+var：
+
+//Variables are simply containers that store values
+//They follow this pattern:
+var yourVariableName = yourValue;
+
+//They can store all of the values we've seen
+var name = "Rusty";
+var secretNumber = 73;
+var isAdorable = true;
+
+//Recall the stored value by calling the variable name
+var name = "Rusty";
+"hello there " + name    //"hello there Rusty"
+
+var num = 37;
+num + 3 + 10    //50
+
+//We can also update existing variables
+var name = "Robert";
+name = "Bob";
+
+
+JS可以隨意地將var的value從數字轉成字串沒問題
+
+JS 使用 camelCase 以識別命名時多單字組成的易讀性
+另外還有幾種case：snack_case kebab-case
+
