194154785f7172e5dde937bc425bb81198cc9990
Affe <qwgygu@gmail.com>
2017-11-01 17:49:00 +0800

    Day8-L8

---
+++ Udemy/L8-JSintro.js
@@ -0,0 +1,30 @@
+JavaScript 的型態
+*Number （數字）
+*String （字串）
+*Boolean （布林）
+*Object （物件）
+	Function （函式）
+	Array （陣列）
+	Date （日期）
+	RegExp
+*Null （空）
+*Undefined （未定義）
+
+
+
+
+基本用法：
+
+Number （數字）：可＋－＊／  ％為餘數  10%3 = 1
+
+String （字串）：可相加 雙引號單引號皆可，但不可混用
+				backslash \ 為escape characters 可將特殊符號如雙引單引號排除於系統命令之外 當成字串顯示
+				下一個字會在但\本身會不見。
+				// ex: "hi \"goodbye\" " －－－> "hi "goodbye" "
+
+
+				計算字串長度 從１開始算
+				"good bye".length ＝８ (空白鍵也會計算計進去)
+
+				顯示字串之第n個數 從０開始算
+				"hello"[0] ＝０
