978863f92a92ac8c4c400f69fed379c09bcdbf33
Affe <qwgygu@gmail.com>
2017-11-10 02:40:32 +0800

    DAY14-L14

---
+++ Udemy/L14.4-JS-todolist.js
@@ -0,0 +1,18 @@
+var list = document.querySelectorAll("li");
+
+
+for (var i = 0; i <= list.length - 1; i++) {
+	list[i].addEventListener("mouseover", function(){
+	this.classList.add("selected")
+});
+
+
+	list[i].addEventListener("mouseout", function(){
+	this.classList.remove("selected")
+});
+
+	list[i].addEventListener("click", function(){
+	this.classList.toggle("done");
+});
+}
+
