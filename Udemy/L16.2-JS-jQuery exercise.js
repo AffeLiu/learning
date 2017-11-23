b7e926a97ece0ba15e897a9cd08dd856390bf4ed
Affe <qwgygu@gmail.com>
2017-11-24 02:46:47 +0800

    day18

---
+++ Udemy/L16.2-JS-jQuery exercise.js
@@ -0,0 +1,14 @@
+// all divs purple background
+$("div").css("background", "purple")
+
+// divs with class "highlight" and make them 200px wide
+$(".highlight").css("width", "200px")
+
+// div with id "third" and give it a orange border
+$("#third").css("border", "2px solid orange")
+
+// first div only and change its font color to pink
+$("div:first-of-type").css("color","pink")
+
+
+// 在 CSS 虛擬類別（pseudo-class）中，:first-of-type 代表本節點為兄弟節點中第一個此類型節點。
